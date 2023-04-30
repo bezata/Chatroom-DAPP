653ee6221cf6c674dc66787c6559bdfbc792302e
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2023-04-30 12:21:44 +0300

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,29 @@
 import "@/styles/globals.css";
-import Wagmi from "./CleanProvider";
+import { getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { celo, celoAlfajores } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+const { chains, provider } = configureChains(
+  [celoAlfajores, celo],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "CeloChat",
+  projectId: "6e18bca83b6d8c08562669f22a83ca97",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }) {
-  return;
-  <Wagmi>
-    <Component {...pageProps} />
-  </Wagmi>;
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
