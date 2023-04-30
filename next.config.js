8223dded067b06d2188b78a4a5f939c8494c0d30
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2023-04-30 13:11:51 +0300

    Redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/Chat",
+        permanent: true,
+      },
+      {
+        source: "/Chat",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
