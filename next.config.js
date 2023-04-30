f04d613d929ed73eacbac2b64a8866dbddc483c9
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2023-04-30 13:20:52 +0300

    jsut using netlify vercel sucks

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/Chat",
-        permanent: false,
-      },
-      {
-        source: "/Chat",
-        destination: "/",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
